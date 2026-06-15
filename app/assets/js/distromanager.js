8ab928ee496d97f75786d7c9c77d2f7dedc33361
Xenows76 <75766022+Xenows76@users.noreply.github.com>
2026-06-15 13:48:33 +0200

    fix: point distribution URL to exe-and-chill.fr VPS

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://90.108.32.193/nebula/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://exe-and-chill.fr/nebula/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
